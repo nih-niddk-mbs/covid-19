9014786fd4a6ede4796a11cbbf14b2ede644e628
aoschwartz7 <aoschwartz7@gmail.com>
2020-08-25 14:45:08 -0400

    added new quantile values for FDA (?) so it can be captured in flag if necessary

--- scripts/make-tables.py
+++ scripts/make-tables.py
@@ -52,7 +52,6 @@ parser.add_argument('-ao', '--average-only', type=int, default=0,
                           'the concatenated (raw) and reweighted tables'))
 args = parser.parse_args()
 
-# If percentiles are required, change quantiles to percentiles
 percentiles = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10,
                0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20,
                0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.30,
@@ -63,8 +62,18 @@ percentiles = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10,
                0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.80,
                0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89, 0.90,
                0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99]
+
+FDA_quantiles = [0.01, 0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4,
+                 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9,
+                 0.95, 0.975, 0.99]
+
+# If percentiles are required, change quantiles to percentiles
 if args.quantiles == ['percentiles']:
     args.quantiles=percentiles
+
+# If quantiles need to match FDA's (?) quantiles, change quantiles
+if args.quantiles == ['FDA_quantiles']:
+    args.quantiles=FDA_quantiles
 
 # If no model_names are provided, use all of them
 if not args.model_names:
