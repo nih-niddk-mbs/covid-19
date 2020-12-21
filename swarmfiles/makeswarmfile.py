f0f1e20571f02a814946ac0d9e516d07b58ff614
alec <aoschwartz7@gmail.com>
2020-12-21 13:38:57 -0500

    string formattiung

--- swarmfiles/makeswarmfile.py
+++ swarmfiles/makeswarmfile.py
@@ -1,23 +1,29 @@
 import glob
 import os
-import re
 
+# Create list of US ROIs found in data_path
 data_path = '/Users/schwartzao/Documents/GitHub/covid-sicr/data/'
 US_rois = []
-for name in glob.glob(data_path + 'covidtimeseries_US_??.csv'): # focusing on US
+for name in glob.glob(data_path + 'covidtimeseries_US_??.csv'):
     roi = os.path.basename(name)
-    roi = re.search('[US_..]', roi)
-    print(roi)
+    US_rois.append(roi[16:-4])
+US_rois.sort()
 
-# rois_path =  # remember to change for Biowulf
-# # roi_files = os.listdir(rois_path)
-# print(roi_files)
-# rois =
+# models = ['SICRdiscrete', 'SICRdiscrete1', 'SICRMQC2R2DXvb']
 
+swarmFile = open("test.swarm", "w")
 
+line = ("source /data/schwartzao/conda/etc/profile.d/conda.sh "
+        "&& conda activate covid "
+        "&& python /home/schwartzao/covid-sicr/scripts/run.py SICRdiscrete "
+        "-r='{}' "
+        "-mp='/data/schwartzao/covid-sicr/models/' "
+        "-fp='/data/schwartzao/covid-sicr/fits/' "
+        "--it=10000	-wm=6000	-f=1	-ad=.85	-ft=1"
+        )
 
+for roi in US_rois:
+    swarmFile.write(line.format(roi))
+    swarmFile.write('\n')
 
-#
-# source /data/schwartzao/conda/etc/profile.d/conda.sh \
-# && conda activate covid \
-# && python /home/schwartzao/covid-sicr/scripts/run.py SICRdiscrete -r='Afghanistan' -mp='/data/schwartzao/covid-sicr/models/' -dp='/home/schwartzao/covid-sicr/data/' -fp='/data/schwartzao/covid-sicr/fits/' -it=10000	-wm=6000	-f=1	-ad=.85	-ft=1
+swarmFile.close()
