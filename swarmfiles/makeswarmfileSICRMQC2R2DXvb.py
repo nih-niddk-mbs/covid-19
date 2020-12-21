d6afa205385d797a96f66b59ccca0e68247f4df1
alec <aoschwartz7@gmail.com>
2020-12-21 14:42:17 -0500

    make swarm files for 3 models
    python scripts for making US States ROIs swarm files for 3 models: SICRMQC2R2DXvb, SICRdiscrete, SICRdiscrete1

--- swarmfiles/makeswarmfileSICRMQC2R2DXvb.py
+++ swarmfiles/makeswarmfileSICRMQC2R2DXvb.py
@@ -2,24 +2,22 @@ import glob
 import os
 
 # Create list of US ROIs found in data_path
-data_path = '/Users/schwartzao/Documents/GitHub/covid-sicr/data/'
+data_path = './data/'
 US_rois = []
 for name in glob.glob(data_path + 'covidtimeseries_US_??.csv'):
     roi = os.path.basename(name)
     US_rois.append(roi[16:-4])
 US_rois.sort()
 
-# models = ['SICRdiscrete', 'SICRdiscrete1', 'SICRMQC2R2DXvb']
-
-swarmFile = open("test.swarm", "w")
+swarmFile = open("run.swarm", "w")
 
 line = ("source /data/schwartzao/conda/etc/profile.d/conda.sh "
         "&& conda activate covid "
-        "&& python /home/schwartzao/covid-sicr/scripts/run.py SICRdiscrete "
+        "&& python /home/schwartzao/covid-sicr/scripts/run.py SICRMQC2R2DXvb "
         "-r='{}' "
         "-mp='/data/schwartzao/covid-sicr/models/' "
         "-fp='/data/schwartzao/covid-sicr/fits/' "
-        "--it=10000	-wm=6000	-f=1	-ad=.85	-ft=1"
+        "-it=10000 -wm=6000 -f=1 -ad=.85 -ft=1"
         )
 
 for roi in US_rois:
---
+++ swarmfiles/makeswarmfileSICRdiscrete1.py
@@ -0,0 +1,27 @@
+import glob
+import os
+
+# Create list of US ROIs found in data_path
+data_path = './data/'
+US_rois = []
+for name in glob.glob(data_path + 'covidtimeseries_US_??.csv'):
+    roi = os.path.basename(name)
+    US_rois.append(roi[16:-4])
+US_rois.sort()
+
+swarmFile = open("run.swarm", "w")
+
+line = ("source /data/schwartzao/conda/etc/profile.d/conda.sh "
+        "&& conda activate covid "
+        "&& python /home/schwartzao/covid-sicr/scripts/run.py SICRdiscrete1 "
+        "-r='{}' "
+        "-mp='/data/schwartzao/covid-sicr/models/' "
+        "-fp='/data/schwartzao/covid-sicr/fits/' "
+        "-it=10000 -wm=6000 -f=1 -ad=.85 -ft=1"
+        )
+
+for roi in US_rois:
+    swarmFile.write(line.format(roi))
+    swarmFile.write('\n')
+
+swarmFile.close()
