074a534b3a4b9dc47234a6ce563ec33a4b311719
aoschwartz7 <46732667+aoschwartz7@users.noreply.github.com>
2021-05-26 16:13:21 -0400

    Update make-tables.py

--- scripts/make-tables.py
+++ scripts/make-tables.py
@@ -184,7 +184,8 @@ df = df.set_index(['model', 'roi', 'quantile']).sort_index()
 
 # Perform model averaging
 if args.model_averaging:
-    df_weights = ncs.get_loo_weights_for_averaging(args.fits_path, args.models_path, args.fit_format, df)
+    print("Getting weights for model averaging.")
+    df_weights = ncs.get_loo_weights_for_averaging(args.fits_path, args.models_path, df)
     # use df_weights to get samples from fit files and save reweighted fit file
     out = tables_path / ('weights_for_averaging.csv')
     df_weights.to_csv(out)
