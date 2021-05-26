3d2abfd889f420969e93c41fffc296c5642e5fd9
aoschwartz7 <46732667+aoschwartz7@users.noreply.github.com>
2021-05-26 16:54:56 -0400

    a

--- niddk_covid_sicr/analysis.py
+++ niddk_covid_sicr/analysis.py
@@ -397,7 +397,7 @@ def get_loo_weights_for_averaging(fits_path, models_path, tables_path):
         raw_table: pd.DataFrame containing regions and stats.
     Returns:
         df_weights: pd.DataFrame containing model weights per region for applicable regions."""
-    raw_table = pd.read_csv(Path(tables_path) / ('fit_table_raw.csv'))
+    raw_table = pd.read_csv(Path(tables_path) / 'fit_table_raw.csv')
     print(raw_table.columns)
     print(raw_table.head())
     raw_table.set_index(['model', 'roi', 'quantile'], inplace=True)
