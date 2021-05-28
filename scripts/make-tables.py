e9b329a1a51830bf7b4762e2bf314e3a5f4de567
aoschwartz7 <46732667+aoschwartz7@users.noreply.github.com>
2021-05-28 12:50:30 -0400

    Update make-tables.py

--- scripts/make-tables.py
+++ scripts/make-tables.py
@@ -199,6 +199,8 @@ if args.model_averaging: # Perform model averaging using raw fit file
             model_path = ncs.get_model_path(args.models_path, model_name)
             extension = ['csv', 'pkl'][args.fit_format]
             fit_path = ncs.get_fit_path(args.fits_path, model_name, roi)
+            print(roi)
+            print(df_weights)
             df_roi = df_weights[(df_weights['roi'] == roi)]
             model_name_weight = model_name + '_weight'
             weight = df_roi[model_name_weight].values[0]
