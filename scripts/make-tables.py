63f80d0a73c97e14b6b37534a3dc5318ebd8f5dc
aoschwartz7 <46732667+aoschwartz7@users.noreply.github.com>
2021-06-03 14:58:37 -0400

    a

--- scripts/make-tables.py
+++ scripts/make-tables.py
@@ -261,12 +261,9 @@ if args.model_averaging: # Perform model averaging using raw fit file
         reweighted_path = Path(args.tables_path) / ('fit_table_reweighted.csv')
         if reweighted_path.resolve().is_file():
             df_reweighted = pd.read_csv(reweighted_path, index_col=['roi', 'quantile'])
-            print(df_reweighted)
             df_averaged = pd.read_csv(Path(args.tables_path) / 'DiscreteAverage_fit_table.csv')
             df_averaged = df_averaged.reset_index(drop=True).set_index(['roi', 'quantile'])
-            print(df_averaged)
             df_reweighted.update(df_averaged)
-            print(df_reweighted)
             df_reweighted.to_csv(Path(args.tables_path) / 'fit_table_reweighted_and_averaged.csv')
     except:
         print("Could not perform model averaging.")
