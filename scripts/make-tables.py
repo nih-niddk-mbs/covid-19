08794259c158813aef41061ed76372e16d6855e1
aoschwartz7 <46732667+aoschwartz7@users.noreply.github.com>
2021-06-03 11:55:49 -0400

    Update make-tables.py

--- scripts/make-tables.py
+++ scripts/make-tables.py
@@ -247,12 +247,12 @@ if args.model_averaging: # Perform model averaging using raw fit file
         # print("There are %d ROIs applicable for model averaging." % len(combos)/2)
         result = p_map(roi_df, repeat(args), *combos, num_cpus=args.max_jobs)
         out = tables_path / ('DiscreteAverage_fit_table.csv')
-        # tables = [df_ for model_name_, roi, df_ in result
-        #             if model_name_ == 'Discrete1']
-        if len(result) > 1:
-            df_averaged = pd.concat(result)
+        tables = [df_ for model_name_, roi, df_ in result]
+        print(tables)
+        if len(tables) > 1:
+            df_averaged = pd.concat(tables)
         else:
-            df_averaged = result[0]
+            df_averaged = tables
 
         df_averaged.sort_index(inplace=True)
         df_averaged.to_csv(out)
